5589ed12d371c5785ad7302eb6b73716288469b0
neverbot <ivan@neverbot.com>
2024-10-19 15:14:47 +0200

    feat: update to use eslint v9

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default [
   {
     // global ignores
     // folders can only be ignored at the global level, per-cfg you must do: '**/dist/**/*'
-    // ignores: ['**/public/'],
+    ignores: ['**/public/'],
   },
   // general defaults
   js.configs.recommended,
